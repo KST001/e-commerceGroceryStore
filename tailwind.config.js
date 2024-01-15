119f372ce5f59b8be7be8d4222952306f57d53d9
KST001 <khemrajsinght57@gmail.com>
2024-01-15 15:05:15 +0545

    project folder setup, tailwindcss config and react icon

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#179957",
+        accentDark: "#184D47",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
